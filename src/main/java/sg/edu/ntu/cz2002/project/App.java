0e19ce125063e3dd5e9ec21a295bbd54b278f157
ivanwangsa <ivan@wangsacl.com>
2015-03-24 17:58:14 +0800

    reformat codes

--- src/main/java/sg/edu/ntu/cz2002/project/App.java
+++ src/main/java/sg/edu/ntu/cz2002/project/App.java
@@ -2,16 +2,13 @@ package sg.edu.ntu.cz2002.project;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-	public static String returnHelloWorld(){
-		return "Hello World!";
-	}
-	
-    public static void main( String[] args )
-    {
-        System.out.println( returnHelloWorld() );
+public class App {
+    public static String returnHelloWorld() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(returnHelloWorld());
     }
 }
